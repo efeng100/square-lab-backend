2d3cf521211e872f50e719e01192f43cd1b3ec8e
Edward Feng <fenge123@gmail.com>
2021-05-08 06:51:51 -0400

    Modified pool connection for heroku

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-	user: 'postgres',
-	password: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	database: 'square_lab',
-});
+const env = process.env.NODE_ENV || 'development';
+let poolParams;
+if (env === 'development') {
+	poolParams = {
+		user: 'postgres',
+		password: 'postgres',
+		host: 'localhost',
+		port: 5432,
+		database: 'square_lab',
+	};
+} else {
+	poolParams = {
+		connectionString: process.env.DATABASE_URL,
+    ssl: true,
+	}
+}
+
+const pool = new Pool(poolParams);
 
 module.exports = pool;
